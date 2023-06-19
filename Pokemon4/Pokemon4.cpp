ff3a9dd5dcd73b79524f2d81be47afea4e0c4dbe
Lara <laramollaoglu1@gmail.com>
2023-06-19 13:35:27 +0200

    Comments added

--- Pokemon4/Pokemon4.cpp
+++ Pokemon4/Pokemon4.cpp
@@ -4,9 +4,7 @@
 #include <random>
 
 
-/// <summary>
-/// Pokemon class 
-/// </summary>
+/// Pokemon class with names and types.
 class Pokemon {
 public:
     Pokemon(const std::string& name, const std::string& type)
@@ -25,26 +23,28 @@ private:
     std::string type;
 };
 
+
+/// Battle class the rock paper scissors type of fight. type weaknesses.
 class Battle {
 public:
-    static const Pokemon* fight(const Pokemon* pokemon1, const Pokemon* pokemon2) {
-        if (pokemon1->getType() == "fire" && pokemon2->getType() == "grass") {
-            return pokemon1;
+    static const Pokemon* fight(const Pokemon* pokemonOne, const Pokemon* pokemonTwo) {
+        if (pokemonOne->getType() == "fire" && pokemonTwo->getType() == "grass") {
+            return pokemonOne;
         }
-        else if (pokemon1->getType() == "grass" && pokemon2->getType() == "water") {
-            return pokemon1;
+        else if (pokemonOne->getType() == "grass" && pokemonTwo->getType() == "water") {
+            return pokemonOne;
         }
-        else if (pokemon1->getType() == "water" && pokemon2->getType() == "fire") {
-            return pokemon1;
+        else if (pokemonOne->getType() == "water" && pokemonTwo->getType() == "fire") {
+            return pokemonOne;
         }
-        else if (pokemon2->getType() == "fire" && pokemon1->getType() == "grass") {
-            return pokemon2;
+        else if (pokemonTwo->getType() == "fire" && pokemonOne->getType() == "grass") {
+            return pokemonTwo;
         }
-        else if (pokemon2->getType() == "grass" && pokemon1->getType() == "water") {
-            return pokemon2;
+        else if (pokemonTwo->getType() == "grass" && pokemonOne->getType() == "water") {
+            return pokemonTwo;
         }
-        else if (pokemon2->getType() == "water" && pokemon1->getType() == "fire") {
-            return pokemon2;
+        else if (pokemonTwo->getType() == "water" && pokemonOne->getType() == "fire") {
+            return pokemonTwo;
         }
         else {
             return nullptr;
@@ -52,6 +52,7 @@ public:
     }
 };
 
+/// Arena class where we save the the rounds and battles.
 class Arena {
 public:
     static int rounds;
@@ -61,6 +62,8 @@ public:
 int Arena::rounds = 0;
 int Arena::battles = 0;
 
+
+/// Trainer class   
 class Trainer {
 public:
     Trainer(const std::string& name, const std::vector<Pokemon>& pokemons)
@@ -70,8 +73,11 @@ public:
         return name;
     }
 
+
     const Pokemon* get_random_pokemon() const {
+        ///creates an object that is used to obtain a seed for the random number engine
         std::random_device rd;
+        ///This is a random number engine that generates random numbers using the Mersenne Twister algorithm
         std::mt19937 gen(rd());
         std::uniform_int_distribution<> dis(0, pokemons.size() - 1);
         return &pokemons[dis(gen)];
@@ -81,28 +87,30 @@ private:
     std::string name;
     std::vector<Pokemon> pokemons;
 };
-
+/// <summary>
+/// BattleArena
+/// </summary>
 class BattleArena {
 public:
-    static void start_battle(const Trainer& trainer1, const Trainer& trainer2) {
+    static void start_battle(const Trainer& Challenger, const Trainer& opponent) {
         Arena::battles++;
-        const Pokemon* trainer1_pokemon = trainer1.get_random_pokemon();
-        const Pokemon* trainer2_pokemon = trainer2.get_random_pokemon();
-        const std::string& trainer1_name = trainer1.getName();
-        const std::string& trainer2_name = trainer2.getName();
-        const std::string& pokemon1_name = trainer1_pokemon->getName();
-        const std::string& pokemon2_name = trainer2_pokemon->getName();
-        const std::string& pokemon1_type = trainer1_pokemon->getType();
-        const std::string& pokemon2_type = trainer2_pokemon->getType();
+        const Pokemon* Challenger_pokemon = Challenger.get_random_pokemon();
+        const Pokemon* opponent_pokemon = opponent.get_random_pokemon();
+        const std::string& Challenger_name = Challenger.getName();
+        const std::string& opponent_name = opponent.getName();
+        const std::string& pokemonOne_name = Challenger_pokemon->getName();
+        const std::string& pokemonTwo_name = opponent_pokemon->getName();
+        const std::string& pokemonOne_type = Challenger_pokemon->getType();
+        const std::string& pokemonTwo_type = opponent_pokemon->getType();
         const Pokemon* winner = nullptr;
         while (winner == nullptr) {
             std::cout << "Round: " << Arena::rounds + 1 << std::endl;
-            std::cout << trainer1_name << "'s " << pokemon1_name << " vs. " << trainer2_name << "'s " << pokemon2_name << std::endl;
-            winner = Battle::fight(trainer1_pokemon, trainer2_pokemon);
+            std::cout << Challenger_name << "'s " << pokemonOne_name << " vs. " << opponent_name << "'s " << pokemonTwo_name << std::endl;
+            winner = Battle::fight(Challenger_pokemon, opponent_pokemon);
             if (winner == nullptr) {
                 std::cout << "It's a draw!" << std::endl << std::endl;
                 if (Arena::rounds > 0) {
-                    winner = (winner == trainer1_pokemon) ? trainer2_pokemon : trainer1_pokemon;
+                    winner = (winner == Challenger_pokemon) ? opponent_pokemon : Challenger_pokemon;
                     std::cout << "Previous winner " << winner->getName() << " returns to their pokeball." << std::endl << std::endl;
                 }
             }
@@ -113,39 +121,37 @@ public:
         }
 
         std::cout << "Battle over!" << std::endl;
-        if (winner == trainer1_pokemon) {
-            std::cout << trainer1_name << " wins the battle!" << std::endl;
+        if (winner == Challenger_pokemon) {
+            std::cout << Challenger_name << " wins the battle!" << std::endl;
         }
         else {
-            std::cout << trainer2_name << " wins the battle!" << std::endl;
+            std::cout << opponent_name << " wins the battle!" << std::endl;
         }
     }
 };
 
 int main() {
-    std::string trainer1_name;
-    std::string trainer2_name;
+    std::string Challenger_name;
+    std::string opponent_name;
     std::string restart;
 
-    Pokemon pokemon1("Charmander", "fire");
-    Pokemon pokemon2("Bulbasaur", "grass");
-    Pokemon pokemon3("Squirtle", "water");
+    Pokemon pokemonOne("Charmander", "fire");
+    Pokemon pokemonTwo("Bulbasaur", "grass");
+    Pokemon pokemonThree("Squirtle", "water");
 
     std::cout << "Enter Trainer 1's name: ";
-    std::cin >> trainer1_name;
-    Trainer trainer1(trainer1_name, { pokemon1, pokemon2, pokemon3 });
+    std::cin >> Challenger_name;
+    Trainer Challenger(Challenger_name, { pokemonOne, pokemonTwo, pokemonThree });
 
     std::cout << "Enter Trainer 2's name: ";
-    std::cin >> trainer2_name;
-    Trainer trainer2(trainer2_name, { pokemon1, pokemon2, pokemon3 });
+    std::cin >> opponent_name;
+    Trainer opponent(opponent_name, { pokemonOne, pokemonTwo, pokemonThree });
 
     do {
-        BattleArena::start_battle(trainer1, trainer2);
+        BattleArena::start_battle(Challenger, opponent);
 
         std::cout << "Enter (R) to restart or any other key to quit: ";
         std::cin >> restart;
-        Arena::rounds = 0;
-        Arena::battles = 0;
     } while (restart == "R" || restart == "r");
 
     return 0;
